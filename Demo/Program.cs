a968198e4ea081b7e635b82f95e9682d0f916b39
Richard <richard@mail.kway.com.tw>
2024-10-22 13:35:13 +0800

    更新程式碼+註解

--- Demo/Program.cs
+++ Demo/Program.cs
@@ -8,7 +8,9 @@ namespace Demo
         {
             var x = DateTime.Now;
             int rocYear = x.Year - 1911;
-            string rocDate = $"{rocYear}/{x.ToString("MM/dd HH:mm:ss fff", CultureInfo.InvariantCulture)}";
+            string rocDate = $"{rocYear}/{x.ToString("MM/dd HH:mm:ss.fff", CultureInfo.InvariantCulture)}";
+            //CultureInfo.InvariantCulture 代表.NET 中的一個靜態屬性 代表一個不變的文化（Invariant Culture）
+            //通常用於：字串比較、數字-日期格式化與解析、序列化與反序列化
             Console.WriteLine(x);
             Console.WriteLine(rocDate);
         }

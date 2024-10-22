41dedf3cddba9e3ae396fa8650d5dea1dc970d8a
Richard <richard@mail.kway.com.tw>
2024-10-22 13:11:50 +0800

    更新datetime

--- Demo/Program.cs
+++ Demo/Program.cs
@@ -1,12 +1,16 @@
-﻿namespace Demo
+﻿using System.Globalization;
+
+namespace Demo
 {
     internal class Program
     {
         static void Main(string[] args)
         {
             var x = DateTime.Now;
-            
+            int rocYear = x.Year - 1911;
+            string rocDate = $"{rocYear}/{x.ToString("MM/dd HH:mm:ss fff", CultureInfo.InvariantCulture)}";
             Console.WriteLine(x);
+            Console.WriteLine(rocDate);
         }
     }
 }
